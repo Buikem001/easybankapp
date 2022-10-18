c5219dea07d685233700042b6c20618f201547f1
Buikem001 <atuhdave@gmail.com>
2022-10-18 16:42:49 +0100

    upload of easy bank

--- main.js
+++ main.js
@@ -12,4 +12,12 @@ function CloseNav() {
     links.style = "top: -30%";
     hamburgerclose.style = "display: none";
     hamburger.style = "display: block";
-} 
+}
+
+let dateCalculation = new Date("2022-08-23");
+let numberOfDaysToAdd = 93;
+let result = dateCalculation.setDate(dateCalculation.getDate() + numberOfDaysToAdd);
+endDate = new Date(result);
+
+const remainingDays = (Math.abs(endDate - (new Date())))
+console.log(Math.floor(remainingDays / (24 * 60 * 60 * 1000)));
